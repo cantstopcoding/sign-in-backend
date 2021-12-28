7decfa9161f4146c060f7de1a3cb1d2e3ae482f6
cantstopcoding <jonathan_koss@yahoo.com>
2021-12-27 23:21:22 -0500

    seed secon user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-rafael = User.create(name: "Rafael", username: "rafa", password: "pw")
+User.create(name: "Rafael", username: "rafa", password: "pw")
+User.create(name: "Jonathan", username: "Jon", password: "pw")
