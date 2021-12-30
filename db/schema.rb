187249ac1e6cb35875aacf0d5e1acf32f9ee68b5
cantstopcoding <jonathan_koss@yahoo.com>
2021-12-29 21:40:38 -0500

    seed and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_12_28_034220) do
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -19,5 +18,4 @@ ActiveRecord::Schema.define(version: 2021_12_28_034220) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
