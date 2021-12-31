aef741426c0f201ba4de380c572efc9cd0deab58
cantstopcoding <jonathan_koss@yahoo.com>
2021-12-31 18:18:17 -0500

    adjust structure of token

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,10 @@
+require "Auth"
+
 class Api::V1::SessionsController < ApplicationController
   def login
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      render json: { token: Auth.create_token(user) }
+      render json: { token: Auth.create_token({ username: user.username, id: user.id, email: user.email }) }
     else
       render json: { errors: { message: "Unable to find a user with that email or password" } }, status: 500
     end
