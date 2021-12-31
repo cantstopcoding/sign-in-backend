8db35c0d924e524a873b21d9ff32b148ae3982b8
cantstopcoding <jonathan_koss@yahoo.com>
2021-12-31 18:18:53 -0500

    update auth file

--- lib/auth.rb
+++ lib/auth.rb
@@ -1,14 +1,18 @@
-require 'jwt'
+require "jwt"
+require "Dotenv"
+
+Dotenv.load(".env")
 
 class Auth
+  APPLICATION_KEY = ENV["APPLICATION_KEY"]
+  APPLICATION_ALGO = ENV["APPLICATION_ALGO"]
 
-    def self.create_token(user_object)
-        binding.pry
-        # payload = { data: 'test' }
-    end
+  def self.create_token(user_object)
+    payload = { user: JSON.parse(user_object.to_json) }
+    token = JWT.encode(payload, APPLICATION_KEY, APPLICATION_ALGO)
+  end
 
-    def self.decode_token
-
-    end
-
+  def self.decode_token(token)
+    decoded_token = JWT.decode(token, APPLICATION_KEY, true, { algorithm: APPLICATION_ALGO })
+  end
 end
