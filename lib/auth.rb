acd31f2cb51af9cf51f4ee72e492b14d74e1093b
cantstopcoding <jonathan_koss@yahoo.com>
2021-12-30 20:10:55 -0500

    create auth controller

---
+++ lib/auth.rb
@@ -0,0 +1,14 @@
+require 'jwt'
+
+class Auth
+
+    def self.create_token(user_object)
+        binding.pry
+        # payload = { data: 'test' }
+    end
+
+    def self.decode_token
+
+    end
+
+end
