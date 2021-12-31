7e48536e0ddc43f0ad7e8abf4f16ff8f50058535
cantstopcoding <jonathan_koss@yahoo.com>
2021-12-31 17:10:26 -0500

    add book route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post "/signup", to: "users#signup"
       post "/login", to: "sessions#login"
+      get "/books", to: "books#index"
     end
   end
 end
