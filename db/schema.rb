a460835766073552a5c384ecd2063d8cef852243
cantstopcoding <jonathan_koss@yahoo.com>
2021-12-31 17:10:57 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_12_31_030910) do
-
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
@@ -26,5 +25,4 @@ ActiveRecord::Schema.define(version: 2021_12_31_030910) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
   end
-
 end
